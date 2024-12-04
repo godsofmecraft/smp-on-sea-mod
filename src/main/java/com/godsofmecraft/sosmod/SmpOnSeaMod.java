67a779bd343ae9c96e736db756360e5f6f913321
godsofmecraft <godsofmecraft@gmail.com>
2024-12-04 12:18:43 +0000

    Initial commit

---
+++ src/main/java/com/godsofmecraft/sosmod/SmpOnSeaMod.java
@@ -0,0 +1,16 @@
+package com.godsofmecraft.sosmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SmpOnSeaMod implements ModInitializer {
+	public static final String MOD_ID = "sosmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
